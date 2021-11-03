60a843187f579be49b7f140f360cd99027968d07
Israel Cena <israelsjm@gmail.com>
2021-11-02 23:54:13 -0300

    rework amenities

--- src/js/utl/amenities.js
+++ src/js/utl/amenities.js
@@ -1,22 +1,25 @@
-export const amenities = {
-	PARTY_HALL: "Salão de Festas",
-	FURNISHED: "Mobiliado",
-	FIREPLACE: "Lareira",
-	POOL: "Piscina",
-	BARBECUE_GRILL: "Churrasqueira",
-	AIR_CONDITIONING: "Ar condicionado",
-	ELEVATOR: "Elevador",
-	PLAYGROUND: "Playground",
-	GATED_COMMUNITY: "Condomínio fechado",
-	SPORTS_COURT: "Quadra poli-esportiva",
-	PETS_ALLOWED: "Aceita animais",
-	LAUNDRY: "Lavanderia",
-	TENNIS_COURT: "Quadra de tênis",
-	GYM: "Academia",
-	AMERICAN_KITCHEN: "Cozinha americana",
-	SAUNA: "Sauna",
-	CINEMA: "Cinema",
-	ELECTRONIC_GATE: "Portão eletrônico",
-	GARDEN: "Jardim",
-	BICYCLES_PLACE: "Bicicletário",
+export const amenitiesTranslator = (item) => {
+	const tagsTranslator = [];
+	tagsTranslator["PETS_ALLOWED"] = "Aceita animais";
+	tagsTranslator["ELEVATOR"] = "Elevador";
+	tagsTranslator["ELECTRONIC_GATE"] = "Portão eletrônico";
+	tagsTranslator["CINEMA"] = "Cinema";
+	tagsTranslator["GYM"] = "Academia";
+	tagsTranslator["PLAYGROUND"] = "Playground";
+	tagsTranslator["GATED_COMMUNITY"] = "Condomínio";
+	tagsTranslator["SAUNA"] = "	Sauna";
+	tagsTranslator["GARDEN"] = "Jardim";
+	tagsTranslator["PARTY_HALL"] = "Salão de Festas";
+	tagsTranslator["FURNISHED"] = "Mobiliádo";
+	tagsTranslator["FIREPLACE"] = "Lareira";
+	tagsTranslator["POOL"] = "Piscina";
+	tagsTranslator["BICYCLES_PLACE"] = "Bicicletário";
+	tagsTranslator["AIR_CONDITIONING"] = "Ar Condicionado";
+	tagsTranslator["SPORTS_COURT"] = "Quadra poli-esportiva";
+	tagsTranslator["AMERICAN_KITCHEN"] = "Cozinha americana";
+	tagsTranslator["TENNIS_COURT"] = "Quadra de tênis";
+	tagsTranslator["LAUNDRY"] = "Lavanderia";
+	tagsTranslator["BARBECUE_GRILL"] = "Churrasqueira";
+
+	return tagsTranslator[item];
 };
